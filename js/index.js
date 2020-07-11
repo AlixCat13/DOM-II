110bfca69aff87965205319b2d37e4be185bdede
Cody C <alix.henry@ymail.com>
2020-07-10 20:42:57 -0500

    started the project. Have 2 of 10 done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+const heading = document.querySelector('.logo-heading')
+heading.addEventListener('dblclick', (event) => {
+    event.target.style.color = 'pink'
+})
+
+const funBus = document.querySelector('.intro img')
+console.log(funBus);
+funBus.addEventListener('mouseover', () => {
+    funBus.style.transform = 'scale(1.2)'
+    funBus.style.transition = 'all 0.3s'
+})
+
+funBus.addEventListener('mouseleave', () => {
+    funBus.style.transform = 'scale(1.0)'
+})
+
+const introHead = document.querySelector('.intro h2')
+introHead.addEventListener()
