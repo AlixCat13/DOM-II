13ba6233b0aa30492352bc2f04e88cd89c0864ef
Cody C <alix.henry@ymail.com>
2020-07-10 22:31:54 -0500

    trying to get some new events working

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 // Your code goes here
+
+//(1)double click
 const heading = document.querySelector('.logo-heading')
 heading.addEventListener('dblclick', (event) => {
     event.target.style.color = 'pink'
 })
 
+//(2) mouse over
 const funBus = document.querySelector('.intro img')
 console.log(funBus);
 funBus.addEventListener('mouseover', () => {
@@ -15,7 +18,7 @@ funBus.addEventListener('mouseleave', () => {
     funBus.style.transform = 'scale(1.0)'
 })
 
-
+//(3) scale / prevent default
 function zoom(event) {
     event.preventDefault();
 
@@ -30,10 +33,11 @@ function zoom(event) {
 
 let scale = .2;
 
-
+//(4) zoom
 const introHead = document.querySelector('.intro h2')
 introHead.onwheel = zoom;
 
+//(5/6) focus / blur
 const destination = document.querySelector('.content-destination')
 destination.addEventListener('focus', (event) => {
     event.target.style.background = 'yellow'
@@ -42,9 +46,34 @@ destination.addEventListener('blur', (event) => {
     event.target.style.background = '';
 });
 
+//(7) resize
 const boatImg = document.querySelector('.content-destination img')
 window.addEventListener('resize', () => {
     boatImg.src = 'https://isorepublic.com/wp-content/uploads/2018/11/isorepublic-cruise-breakfast-1-1100x734.jpg'
 })
+
+//(8)click
+
+const button = document.querySelectorAll('.btn')
+console.log(button)
+button[1].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'yellow'
+})
+
+
+
+
+
+//(9)
+
+
+
+
+//(10)
+
+
+
+
+
 
 
